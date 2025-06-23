d0dedd6a9a412e22fec14505347e2eb1e4f22a55
liviatoledob <liviatoledo.frc8066@gmail.com>
2025-06-23 00:10:15 -0300

    Da onde foi que essa pasta de json surgiu?

---
+++ db/titulos.js
@@ -0,0 +1,9 @@
+let tituloslivia = [
+    "NOSSOS EMBAIXADORES",
+    "CONTEÚDO ESPECIAL",
+    "RELATOS",
+    "Vídeo mais recente do nosso canal",
+    "CONTATE-NOS",
+    "QUER SER NOSSO PARCEIRO?",
+    "POSTAGENS"
+]
