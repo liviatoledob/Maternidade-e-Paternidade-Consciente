8663ab6ae41ecbf3d9d94c5e14d30a15ef0f8e74
Wallace Rodrigues Eduardo <166111524+WallaceDevHub@users.noreply.github.com>
2025-06-24 14:29:28 -0300

    EMBAIXADORES E POSTS - TESTE FUNCIONAMENTO

---
+++ js/app.js
@@ -0,0 +1,53 @@
+
+
+let state = {
+    p: document.getElementById("text-flavia"),
+    url: document.getElementById("img-flavia"),
+    embaixadores: document.getElementById("embaixadores"),
+    carousel: document.getElementById("carousel"),
+    posts: document.getElementById("posts-cards"),
+}
+
+function carregar() {
+    for (let i = 0; i < 4; i++) {
+        console.log(frasesMotivacionais[i].frase);
+        state.embaixadores.innerHTML += `<div class="embaixador">
+        <img src="${catalogoFilmesSeries[i].imagem}" alt="Flávia Calina">
+        <h5>${frasesMotivacionais[i].autor}</h5><!--PERGUNTAR PRO WALLACE SE É PRA PEGAR DO ARRAY OU NÃO-->
+        <p>${frasesMotivacionais[i].frase}</p><!--PARAGRAFO CURTO ARRAY-->
+      </div>`
+    }
+
+}
+
+function carregarposts() {
+    for (let i = 0; i < 8; i++) {
+        console.log(frasesMotivacionais[i].data);
+        state.posts.innerHTML += `<div class="card" style="width: 18rem;">
+            <img src="${catalogoFilmesSeries[i].imagem}" class="card-img-top" alt="Imagem card"><!--PEGAR IMAGEM DO ARRAY-->
+            <div class="card-body">
+              <small>${frasesMotivacionais[i].data}</small><!--PEGAR DATA DO ARRAY-->
+              <h6 class="card-title fw-bold mt-2">AS FÉRIAS ESTÃO CHEGANDO: ideias para entreter as crianças</h6><!--ISSO É UM TÍTULO OU NÃO É? NÃO SEI, MAS PRECISA PEGAR DO ARRAY DADOS OU TITULOS-->
+              <p class="card-text small">${artigos[i].paragrafo01}</p><!--PEGAR PARÁGRAFO DO ARRAY-->
+              <a href="#" class="btn btn-light btn-sm">Ver artigo &gt;</a>
+            </div>
+          </div>`
+    }
+
+
+
+
+
+
+
+
+}
+
+if(state.embaixadores){
+  carregar()  
+}
+
+
+if(state.posts){
+    carregarposts()
+}
