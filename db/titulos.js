5c24bac6be2634ffb0a83bfa1891c6f72d743648
liviatoledob <liviatoledo.frc8066@gmail.com>
2025-06-24 23:06:26 -0300

    FINALIZAÇAO SITE

--- db/titulos.js
+++
@@ -1,9 +0,0 @@
-let tituloslivia = [
-    "NOSSOS EMBAIXADORES",
-    "CONTEÚDO ESPECIAL",
-    "RELATOS",
-    "Vídeo mais recente do nosso canal",
-    "CONTATE-NOS",
-    "QUER SER NOSSO PARCEIRO?",
-    "POSTAGENS"
-]
